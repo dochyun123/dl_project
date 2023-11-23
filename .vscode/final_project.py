6f0ee69e0159d536c85cf0d158188de03ed07420
dochyun123 <dochyun123@gmail.com>
2023-11-23 13:46:40 +0900

    BS

--- .vscode/final_project.py
+++ .vscode/final_project.py
@@ -1,7 +1,6 @@
 # let's start again
 import pandas as pd
 
-data = pd.read_excel("C:/Users/dochy/Desktop/고려대/딥러닝/project/fsdata/2023_1Q_BS.xlsx")
-print(data.head())
-print(data.describe())
-print(data.iloc[0:10])
+data = pd.read_excel("2022_data/2022_consolidated/2022_BS.xlsx")
+unique_values = data['항목명'].unique()
+print(unique_values)
