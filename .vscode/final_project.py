f078f3805d823b96ba6c407721d4cbec5881b36e
dochyun123 <dochyun123@gmail.com>
2023-12-08 02:35:48 +0900

    ratio_test

--- .vscode/final_project.py
+++ .vscode/final_project.py
@@ -1,119 +1,17 @@
-# let's start again
-import pandas as pd
-
-data = pd.read_excel(
-    "C:/Users/dochy/Desktop/고려대/딥러닝/project/fsdata/2023_1Q_BS_filter.xlsx"
-)
-unique_values = data["항목명"].unique()
-print(unique_values)
-
-import pandas as pd
-
-
-class DataFrameProcessor:
-    def __init__(
-        self, source_df, target_df
-    ):  # source : original data # target : new dataframe
-        self.source_df = source_df
-        self.target_df = target_df
-
-    def clean_column(self, column_name):  # column_name '항목명'
-        non_korean_regex = r"[^\uAC00-\uD7AF]"
-        pattern = r"[^\uAC00-\uD7AF]|[\s]"
-        self.source_df[column_name] = self.source_df[column_name].str.replace(
-            non_korean_regex, "", regex=True
-        )
-        self.source_df[column_name] = self.source_df[column_name].str.replace(
-            pattern, "", regex=True
-        )
-
-    def update_with_item(self, item_name, target_column_name, period):
-        # period : 당기 or 전기 등
-        for index, row in self.target_df.iterrows():
-            value = self.source_df[
-                (self.source_df["회사명"] == row["회사명"])
-                & (self.source_df["항목명"] == item_name)
-            ][period]
-            if not value.empty:
-                self.target_df.at[index, target_column_name] = value.iloc[0]
-
-    def fill_missing_values(self, column_to_fill, operation):
-        replace_value = operation(self.target_df)
-        self.target_df[column_to_fill] = self.target_df[column_to_fill].fillna(
-            replace_value
-        )
-
-    def get_modified_target_df(self):
-        return self.target_df
-
-
-# Initialize the processor with your dataframes
-source_df = data
-columns = [
-    "회사명",
-    "당기_유동자산",
-    "당기_비유동자산",
-    "당기_자본",
-    "당기_유동부채",
-    "당기_비유동부채",
-    "당기_부채 및 자본총계",
-    "당기_부채총계",
-    "당기_자산총계",
-]
-target_df = pd.DataFrame(columns=columns)
-processor = DataFrameProcessor(source_df, target_df)
-
-# Clean the '항목명' column
-processor.clean_column("항목명")
-
-# Update df_2022_BS with various items
-items_to_update = [
-    ("유동자산", "당기_유동자산"),
-    ("비유동자산", "당기_비유동자산"),
-    ("유동부채", "당기_유동부채"),
-    ("비유동부채", "당기_비유동부채"),
-    ("자본총계", "당기_자본"),
-    ("부채및자본총계", "당기_부채 및 자본총계"),
-    ("부채총계", "당기_부채총계"),
-    ("자산총계", "당기_자산총계"),
-]
-
-for item_name, target_column_name in items_to_update:
-    processor.update_with_item(item_name, target_column_name, "당기 1분기말")
-
-# Fill missing values
-operation2 = lambda target_df: target_df["당기_부채총계"] - target_df["당기_유동부채"]
-processor.fill_missing_values("당기_비유동부채", operation2)
-
-operation3 = lambda target_df: target_df["당기_부채총계"] - target_df["당기_비유동부채"]
-processor.fill_missing_values("당기_유동부채", operation3)
-
-print(target_df)
-print("finish")
-
-
-def merge_with_label(data, label):
-    # Clean the '종목코드' column in 'data' DataFrame
-    data["종목코드"] = data["종목코드"].str.replace("[", "").str.replace("]", "")
-
-    # Merge with the label DataFrame
-    merged_df = pd.merge(data, label, on="종목코드", how="inner")
-
-    return merged_df
-
-
 import pandas as pd
 
 # 1step : preprocess
 
 BS_item = ["유동자산", "비유동자산", "자산총계", "자본총계", "부채및자본총계", "유동부채", "비유동부채", "부채총계"]
 CF_item = ["영업활동현금흐름"]
-IC_item = ["매출액", "매출총이익", "순이익"]
+PL_item = ["매출액", "매출총이익", "순이익"]
 
 
-def preprocess(
-    file_path, item_names_to_extract
-):  # item_names 에 BS_item / CF_item / IC_item 넣기
+def select_non(series):
+    return series.dropna().iloc[0] if not series.dropna().empty else None
+
+
+def preprocess(file_path, item_names_to_extract):
     # Load the data
     data = pd.read_excel(file_path)
 
@@ -128,11 +26,17 @@ def preprocess(
 
     # Extract the specified item names
     extracted_data = data[data["항목명"].isin(item_names_to_extract)]
-    extracted_data = extracted_data.pivot(
-        index=["회사명", "종목코드"], columns="항목명", values="당기 1분기"
+    grouped_data = (
+        extracted_data.groupby(["회사명", "종목코드", "항목명"])
+        .agg({"당기": select_non})
+        .reset_index()
+    )
+
+    final_data = grouped_data.pivot(
+        index=["회사명", "종목코드"], columns="항목명", values="당기"
     ).reset_index()
 
-    return extracted_data
+    return final_data
 
 
 # 2nd step : fillna preprocessed dataframe
@@ -168,3 +72,43 @@ def create_class_and_merge(data_df, label_df, drop_columns):
     merged_df = pd.merge(data_df, label_df, on=["종목코드"], how="outer")
 
     return merged_df
+
+
+filterd_BS_1Q_2020 = preprocess("2020_1Q_BS_filter.xlsx", BS_item)
+filterd_CF_1Q_2020 = preprocess("2020_1Q_CF_filter.xlsx", CF_item)
+filterd_PL_1Q_2020 = preprocess("2020_1Q_PL_filter.xlsx", PL_item)
+
+filterd_BS_1Q_2020 = fillnan(filterd_BS_1Q_2020)
+filterd_CF_1Q_2020 = fillnan(filterd_CF_1Q_2020)
+filterd_PL_1Q_2020 = fillnan(filterd_PL_1Q_2020)
+
+df_1Q_2020 = merge(filterd_BS_1Q_2020, filterd_CF_1Q_2020, filterd_PL_1Q_2020)
+df_1Q_2020.to_csv("df_1Q_2020")
+
+
+filterd_BS_1Q_2023 = preprocess("2023_1Q_BS_filter.xlsx", BS_item)
+filterd_CF_1Q_2023 = preprocess("2023_1Q_CF_filter.xlsx", CF_item)
+filterd_PL_1Q_2023 = preprocess("2023_1Q_PL_filter.xlsx", PL_item)
+
+filterd_BS_1Q_2023 = fillnan(filterd_BS_1Q_2023)
+filterd_CF_1Q_2023 = fillnan(filterd_CF_1Q_2023)
+filterd_PL_1Q_2023 = fillnan(filterd_PL_1Q_2023)
+
+df_1Q_2020 = merge(filterd_BS_1Q_2023, filterd_CF_1Q_2023, filterd_PL_1Q_2023)
+df_1Q_2020.to_csv("df_1Q_2023")
+
+
+def calculate_financial_ratios(df):
+    # Calculating each ratio
+    df["Current Ratio"] = df["유동자산"] / df["유동부채"]
+    df["Quick Ratio"] = (df["유동자산"] - df["재고자산"]) / df["유동부채"]
+    df["Debt Ratio"] = df["부채 총계"] / df["자본총계"]
+    df["Fixed Asset Turnover"] = df["매출액"] / df["비유동자산"]
+    df["Equity Turnover"] = df["매출액"] / df["자산총계"]
+    df["ROE"] = df["순이익"] / df["자본총계"]
+    df["ROA"] = df["순이익"] / df["자산총계"]
+    df["Gross Margin Ratio"] = df["매출 총이익"] / df["매출액"]
+    df["Operating Cash Flow Ratio"] = df["영업활동현금흐름"] / df["유동부채"]
+    df["Cash Flow Coverage Ratio"] = df["영업활동현금흐름"] / df["부채 총계"]
+    df["Cash Flow Margin Ratio"] = df["영업활동현금흐름"] / df["순이익"]
+    return df
